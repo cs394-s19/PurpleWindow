083256d772ab3a01865bf593273beaf42af669d5
Marblez <MatthewWang2020@u.northwestern.edu>
2019-04-05 20:43:40 -0500

    Review Component

--- purplewindow/src/components/JobScreen.js
+++ purplewindow/src/components/JobScreen.js
@@ -13,7 +13,6 @@ class JobScreen extends Component {
         </div>
         <p>{this.props.job.title}</p>
         <p>{this.props.job.rating}</p>
-        <Review reviewDetails={this.props} index='0'/>
         {/* NOTE: CHECK APP.JS TO SEE WHAT OTHER JOB INFORMATION WAS PASSED DOWN */}
       </div>
     );
