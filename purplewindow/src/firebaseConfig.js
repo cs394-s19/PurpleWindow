e42a8c48dad34df9e87d33e6e2a96946c0a25397
Allan <allanfarkash2020@u.northwestern.edu>
2019-04-26 15:41:17 -0500

    import firebase credentials

---
+++ purplewindow/src/firebaseConfig.js
@@ -0,0 +1,11 @@
+  // Initialize Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyD26AQhzl73lJeBzGZ4EsSOKcpICv2FA1o",
+    authDomain: "purplewindow-3acf0.firebaseapp.com",
+    databaseURL: "https://purplewindow-3acf0.firebaseio.com",
+    projectId: "purplewindow-3acf0",
+    storageBucket: "purplewindow-3acf0.appspot.com",
+    messagingSenderId: "794968149806"
+  };
+
+  export default firebaseConfig;
